d25f09c2539b0ef4dcf87151e6ff6779f6a6941b
Yoqut <bovaqulovi@gmail.com>
2026-04-10 17:49:25 +0500

    update

--- config/settings.py
+++ config/settings.py
@@ -5,7 +5,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 SECRET_KEY = config('SECRET_KEY', default='dev-secret-key-change-in-production')
 DEBUG = config('DEBUG', default=True, cast=bool)
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1', cast=Csv())
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1,0.0.0.0,testserver', cast=Csv())
 
 INSTALLED_APPS = [
     'django.contrib.admin',
